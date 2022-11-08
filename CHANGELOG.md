d120c0548e2ade25d26d5561173f26289dbb1108
Cedric McDougal <cedric@neighborschools.com>
2022-11-08 07:04:24 -0500

    Support reading input from stdin

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,57 +1,63 @@
-# 3.4.0 / 2022-11-04
+# CHANGELOG
+
+## 3.5.0 / 2022-11-08
+
+- Support reading input from stdin
+
+## 3.4.0 / 2022-11-04
 
 - Treat tsconfig paths that leave the root directory as external imports
 - Fix bug where tsconfig paths with forward slashes were not getting parsed correctly
 
-# 3.3.1 / 2022-07-30
+## 3.3.1 / 2022-07-30
 
 - Add `node:url` to Node built-ins array
 
-# 3.3.0 / 2022-06-03
+## 3.3.0 / 2022-06-03
 
 - Support TypeScript config files for resolving import aliases
 
-# 3.2.0 / 2020-10-24
+## 3.2.0 / 2020-10-24
 
 - Add `--ignore` flag for ignoring files
 
-# 3.1.1 / 2020-10-24
+## 3.1.1 / 2020-10-24
 
 - Fix bug where namespace imports were merged with non-namespace imports, causing the output to have a syntax error
 
-# 3.1.0 / 2020-01-15
+## 3.1.0 / 2020-01-15
 
 - Support sorting of TypeScript files
 
-# 3.0.0 / 2020-01-12
+## 3.0.0 / 2020-01-12
 
 BREAKING CHANGES:
 
 1. The CLI tool now prints the file modifications to stdout by default instead of writing directly to the file. To write to the file, the `--write` flag is now required.
 2. The `--config-path` flag was renamed to `--config`
 
-# 2.1.0 / 2020-01-12
+## 2.1.0 / 2020-01-12
 
 - Fixed bug where import alias cache would apply to other projects using js-isort
 - Updated all dependencies to latest versions
 
-# 2.0.4 / 2018-09-22
+## 2.0.4 / 2018-09-22
 
 - Added CHANGELOG
 
-# 2.0.3 / 2018-09-22
+## 2.0.3 / 2018-09-22
 
 - Added support for `dynamicImport` syntax to babel parser
 
-# 2.0.2 / 2018-08-31
+## 2.0.2 / 2018-08-31
 
 - Updated `--help` text
 
-# 2.0.1 / 2018-08-31
+## 2.0.1 / 2018-08-31
 
 - README update
 
-# 2.0.0 / 2018-08-30
+## 2.0.0 / 2018-08-30
 
 **BREAKING CHANGES:**
 
@@ -61,6 +67,6 @@ BREAKING CHANGES:
 
 - Added support for Babel config files
 
-# 1.0.1 / 2018-07-20
+## 1.0.1 / 2018-07-20
 
 - Fixed bug with checking whether module is alias
--- src/js-isort-cli.js
+++ src/js-isort-cli.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/* eslint-disable max-len */
 
 const program = require(`commander`);
 const fs = require(`fs`);
@@ -13,10 +14,14 @@ const collect = (val, memo) => {
 
 program
   .version(`3.2.0`, `-v, --version`)
-  .usage(`[options] <file ...>`)
+  .usage(`[options] [file ...]`)
+  .description(
+    `By default, output is written to stdout.
+Stdin is read if it is piped to js-isort and no files are given.`
+  )
   .option(
     `--config [value]`,
-    `Path to Babel or webpack configuration file containing aliases`
+    `Path to Babel, webpack or TypeScript configuration file containing path aliases`
   )
   .option(`--write`, `Edit files in-place`)
   .option(`--ignore [value]`, `Skip files matching the given path`, collect, [])
@@ -27,39 +32,50 @@ const configPath = program.config;
 const writeToFile = program.write;
 const ignore = program.ignore;
 
-if (filePaths.length === 0) {
-  program.help();
+let stdIn;
+
+try {
+  stdIn = fs.readFileSync(process.stdin.fd, `utf-8`);
+} catch (err) {
+  stdIn = null;
 }
 
 const aliases = resolveAliases(configPath);
 
-filePaths.forEach((filePath) => {
-  const skip =
-    ignore.length > 0 &&
-    ignore.some((pathSubstring) => {
-      return filePath.includes(pathSubstring);
-    });
-
-  if (skip) {
-    return;
-  }
-
-  fs.readFile(filePath, `utf8`, (readError, content) => {
-    if (readError) {
-      throw readError;
-    }
-
-    const sortedContent = isort(content, aliases);
-
-    if (!writeToFile) {
-      // eslint-disable-next-line no-console
-      console.log(sortedContent);
-    } else if (sortedContent !== content) {
-      fs.writeFile(filePath, sortedContent, (writeError) => {
-        if (writeError) {
-          throw writeError;
-        }
+if (filePaths.length > 0) {
+  filePaths.forEach((filePath) => {
+    const skip =
+      ignore.length > 0 &&
+      ignore.some((pathSubstring) => {
+        return filePath.includes(pathSubstring);
       });
+
+    if (skip) {
+      return;
     }
+
+    fs.readFile(filePath, `utf8`, (readError, content) => {
+      if (readError) {
+        throw readError;
+      }
+
+      const sortedContent = isort(content, aliases);
+
+      if (!writeToFile) {
+        // eslint-disable-next-line no-console
+        console.log(sortedContent);
+      } else if (sortedContent !== content) {
+        fs.writeFile(filePath, sortedContent, (writeError) => {
+          if (writeError) {
+            throw writeError;
+          }
+        });
+      }
+    });
   });
-});
+} else if (stdIn) {
+  // eslint-disable-next-line no-console
+  console.log(isort(stdIn, aliases));
+} else {
+  program.help();
+}
